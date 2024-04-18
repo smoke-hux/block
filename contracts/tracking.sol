843e29d1a1a3b9118e1b63cc8a345009f5d77996
smoke-hux <huxleyomondu@live.com>
2024-04-19 01:25:53 +0300

    first commit

--- contracts/tracking.sol
+++ contracts/tracking.sol
@@ -29,4 +29,30 @@ contract Tracking {
         ShipmentStatus status;
         bool isPaid;
     }
+
+    TypeShipment[] Shipments;
+
+    event ShipmentCreated(address indexed sender, address indexed receiver, uint256 pickupTime, uint256 distance, uint256 price);
+    
+    event ShipmentInTransit(address indexed sender, address indexed receiver, uint256 pickupTime);
+
+    event ShipmentDelivered(address indexed sender, address indexed receiver,uint256 deliveryTime);
+
+    event ShipmentPaid(address indexed sender, address indexed receiver, uint256 price);
+
+
+    constructor() {
+        shipmentCount = 0;
+    }
+
+    function createShipmet(address _receiver, uint256 _pickupTime, uint256 _distance, uint_price) public payable {
+        require(msg.value == _price, "Incorrect payment amount");// this is to check if the payment amount is correct
+
+        Shipment memory shipment = shipment(msg.sender, _reciever, _pickupTime, 0, _distance, _price, ShipmentStatus.PENDING, false);// this is the data of the shipment updating
+
+        shipments[msg.sender].push(shipment);// this is to add the shipment to the mapping 
+        shipmentCount++; // this is to keep track of the number of shipment we are tracking
+
+    }
+
 }
