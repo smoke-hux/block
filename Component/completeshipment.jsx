7be93181cccf93b9291bb612354994dc118ba411
smoke-hux <huxleyomondu@live.com>
2024-05-06 03:42:41 +0300

    ss

---
+++ Component/completeshipment.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Completeshipment = () => {
+    return <div>
+        Completeshipment
+    </div>
+};
+
+export default Completeshipment;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
 
+
+// internal imports 
+
+import { Trackingprovider } from "@/Context/tracking";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return ( <>
+  <Trackingprovider> 
+    <Component {...pageProps} /> 
+  </Trackingprovider>
+  </> );
 }
